0d3740d6cbf1bdb8e009f41bfeddcd98fadefa05
Kiran Moktan <kirantamang184@gmail.com>
2024-10-11 03:30:29 +0545

    feat: updated cart to use a function that returns an object

--- data/cart.js
+++ data/cart.js
@@ -1,67 +1,76 @@
-export const cart = {
-    cartItems: undefined,
+export const cart = Cart();
 
-    loadFromStorage() {
-        this.cartItems = JSON.parse(localStorage.getItem('cart'));
+/**
+ * This is function that returns an object. The convention of naming such function is using Pascal case.
+ * 
+ * @returns Object
+ */
+function Cart() {
+    return {
+        cartItems: undefined,
 
-        if (!this.cartItems) {
-            this.cartItems = [
-                {
-                    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-                    quantity: 2,
+        loadFromStorage() {
+            this.cartItems = JSON.parse(localStorage.getItem('cart'));
+
+            if (!this.cartItems) {
+                this.cartItems = [
+                    {
+                        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                        quantity: 2,
+                        deliveryOptionId: '1'
+                    },
+                    {
+                        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+                        quantity: 1,
+                        deliveryOptionId: '2'
+                    }
+                ];
+            }
+        },
+
+        saveToStorage() {
+            localStorage.setItem('cart', JSON.stringify(this.cartItems));
+        },
+
+        addToCart(productId, productQuantity) {
+            let cartItem = null;
+
+            if (cartItem = this.cartItems.find(item => item.productId === productId)) {
+                cartItem.quantity += productQuantity;
+            } else {
+                this.cartItems.push({
+                    productId: productId,
+                    quantity: productQuantity,
                     deliveryOptionId: '1'
-                },
-                {
-                    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-                    quantity: 1,
-                    deliveryOptionId: '2'
-                }
-            ];
-        }
-    },
+                });
+            }
 
-    saveToStorage() {
-        localStorage.setItem('cart', JSON.stringify(this.cartItems));
-    },
+            this.saveToStorage();
+        },
 
-    addToCart(productId, productQuantity) {
-        let cartItem = null;
+        removeFromCart(productId) {
+            this.cartItems = this.cartItems.filter((cartItem) => cartItem.productId !== productId);
 
-        if (cartItem = this.cartItems.find(item => item.productId === productId)) {
-            cartItem.quantity += productQuantity;
-        } else {
-            this.cartItems.push({
-                productId: productId,
-                quantity: productQuantity,
-                deliveryOptionId: '1'
+            this.saveToStorage();
+        },
+
+        getCartQuantity() {
+            let cartQuantity = 0;
+
+            this.cartItems.forEach((cartItem) => {
+                cartQuantity += cartItem.quantity;
             });
+
+            return cartQuantity;
+        },
+
+        updateDeliveryOption(productId, deliveryOptionId) {
+            let cartItem = this.cartItems.find((item) => item.productId === productId);
+            cartItem.deliveryOptionId = deliveryOptionId;
+
+            this.saveToStorage();
         }
-
-        this.saveToStorage();
-    },
-
-    removeFromCart(productId) {
-        this.cartItems = this.cartItems.filter((cartItem) => cartItem.productId !== productId);
-
-        this.saveToStorage();
-    },
-
-    getCartQuantity() {
-        let cartQuantity = 0;
-
-        this.cartItems.forEach((cartItem) => {
-            cartQuantity += cartItem.quantity;
-        });
-
-        return cartQuantity;
-    },
-
-    updateDeliveryOption(productId, deliveryOptionId) {
-        let cartItem = this.cartItems.find((item) => item.productId === productId);
-        cartItem.deliveryOptionId = deliveryOptionId;
-        
-        this.saveToStorage();
-    }
+    };
 };
 
 cart.loadFromStorage();

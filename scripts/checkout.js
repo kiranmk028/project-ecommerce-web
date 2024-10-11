32a8758b68106253f0ce73b4d4e676238b964175
Kiran Moktan <kirantamang184@gmail.com>
2024-10-11 23:42:35 +0545

    feat: refactored all promises in checkout.js to run asynchronously

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,19 +2,19 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 
-new Promise((resolve) => {
-    loadProducts(() => {
-        console.log('Inside promise 1.')
-        resolve('Value from Promise 1.');
-    });
-}).then((value1) => {
-    console.log(value1);
-    return new Promise((resolve) => {
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            console.log('Inside promise 1.')
+            resolve('Value from Promise 1.');
+        });
+    }),
+    new Promise((resolve) => {
         console.log('Inside Promise 2.');
         resolve('Value from Promise 2.');
-    });
-}).then((value2) => {
-    console.log(value2);
+    })
+]).then((values) => {
+    console.log(values);
     renderOrderSummary();
     renderPaymentSummary();
 });

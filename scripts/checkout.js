adf6bb1b476b937f3512dc46efbff504e3b4900f
Kiran Moktan <kirantamang184@gmail.com>
2024-10-11 23:38:29 +0545

    feat: refactored checkout.js to use promise instead of a callback

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,19 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 
-loadProducts(() => {
+new Promise((resolve) => {
+    loadProducts(() => {
+        console.log('Inside promise 1.')
+        resolve('Value from Promise 1.');
+    });
+}).then((value1) => {
+    console.log(value1);
+    return new Promise((resolve) => {
+        console.log('Inside Promise 2.');
+        resolve('Value from Promise 2.');
+    });
+}).then((value2) => {
+    console.log(value2);
     renderOrderSummary();
     renderPaymentSummary();
 });

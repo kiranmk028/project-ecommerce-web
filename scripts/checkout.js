50de08d696f50e5ab4ea9930b269d7f70a106282
Kiran Moktan <kirantamang184@gmail.com>
2024-10-11 23:53:39 +0545

    udpate: cleaned up checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,19 +2,11 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 
-Promise.all([
-    new Promise((resolve) => {
-        loadProducts(() => {
-            console.log('Inside promise 1.')
-            resolve('Value from Promise 1.');
-        });
-    }),
-    new Promise((resolve) => {
-        console.log('Inside Promise 2.');
-        resolve('Value from Promise 2.');
-    })
-]).then((values) => {
-    console.log(values);
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve();
+    });
+}).then(() => {
     renderOrderSummary();
     renderPaymentSummary();
 });
